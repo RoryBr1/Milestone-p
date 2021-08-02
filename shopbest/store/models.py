d28b843a794a8cef24f49930284e5c72d40c8d6d
RoryBr1 <rorybr1@gmail.com>
2021-08-02 23:30:39 +0000

    missing image failsafe added. items rendering in cart.

--- shopbest/store/models.py
+++ shopbest/store/models.py
@@ -14,14 +14,22 @@ class Customer(models.Model):
 
 # Product Model
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.FloatField()
-    digital = models.BooleanField(default=False,null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-	
-    def __str__(self):
-        return self.name
+	name = models.CharField(max_length=200)
+	price = models.FloatField()
+	digital = models.BooleanField(default=False,null=True, blank=True)
+	image = models.ImageField(null=True, blank=True)
 
+	def __str__(self):
+		return self.name
+
+	# Missing image failsafe
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ''
+		return url
 
 # Order Model
 class Order(models.Model):
