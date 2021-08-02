7792a9294cfde98abbef15d4ed99bf5b77be2097
RoryBr1 <rorybr1@gmail.com>
2021-08-02 22:26:51 +0000

    products rendering on store. product images added to product model. Pillow installed and image uploads from admin panel are functional. product image styling improved.

--- shopbest/store/views.py
+++ shopbest/store/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 
 #   Views
-
 def store(request):
-     context = {}
-     return render(request, 'store/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
+
 
 def cart(request):
      context = {}
      return render(request, 'store/cart.html', context)
+
 
 def checkout(request):
       context = {}
