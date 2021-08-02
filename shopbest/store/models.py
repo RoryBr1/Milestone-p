4b9a65cfb32c45ccb07d993f29d02d4cce2cacba
RoryBr1 <rorybr1@gmail.com>
2021-08-02 21:45:37 +0000

    added customer,product,order,orderitem,shipping address model classes. removed phone-number field from checkout (developer decision)

--- shopbest/store/models.py
+++ shopbest/store/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Customer Model
+class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50, null=True)
+	email = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
+
+# Product Model
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False,null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+# Order Model
+class Order(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+	date_ordered = models.DateTimeField(auto_now_add=True)
+	complete = models.BooleanField(default=False)
+	transaction_id = models.CharField(max_length=100, null=True)
+
+	def __str__(self):
+		return str(self.id)
+
+
+# Order Item Model (Items in cart)
+class OrderItem(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	quantity = models.IntegerField(default=0, null=True, blank=True)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+
+# Shipping Address
+class ShippingAddress(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	address1 = models.CharField(max_length=200, null=False)
+	address2 = models.CharField(max_length=200, null=False)
+	county = models.CharField(max_length=200, null=False)
+	eircode = models.CharField(max_length=200, null=False)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.address
