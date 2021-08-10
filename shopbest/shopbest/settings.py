bbbbdbeb493d291aa00cd23ff0e7736d9e59c3c2
RoryBr1 <rorybr1@gmail.com>
2021-08-10 00:19:41 +0000

    fixing heroku deployment error: STATIC_ROOT was not set

--- shopbest/shopbest/settings.py
+++ shopbest/shopbest/settings.py
@@ -146,6 +146,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
