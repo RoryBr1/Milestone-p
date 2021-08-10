243757dd354ac7913cc8571fa0f52dc11c28ae62
RoryBr1 <rorybr1@gmail.com>
2021-08-10 13:02:32 +0000

    installed django-heroku. attempting to fix H10 heroku error.

--- shopbest/shopbest/settings.py
+++ shopbest/shopbest/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -155,3 +156,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Leave at bottom:
+django_heroku.settings(locals())
