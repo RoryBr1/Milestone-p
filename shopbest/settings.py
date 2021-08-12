0e833fd9f42f59fd99ae9b7f8a0b73257e00ef85
RoryBr1 <rorybr1@gmail.com>
2021-08-12 11:25:44 +0000

    editing settings.py and requirements.txt for heroku

--- shopbest/settings.py
+++ shopbest/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import django_heroku
 from pathlib import Path, os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,11 +99,15 @@ WSGI_APPLICATION = 'shopbest.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://dclozccdlxcjii:a1eb04a2958581d91b25ed356f29dcb541e6b0c912ea10707415e137d3275c60@ec2-63-33-14-215.eu-west-1.compute.amazonaws.com:5432/d9d9ghr1f0l9qh')
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
